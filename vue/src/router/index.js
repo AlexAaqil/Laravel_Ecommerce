8b6318468b47783e07f4ac2db9a543637a8cdf7a
alexaaqil <alexaaqil.se@gmail.com>
2024-02-06 15:15:52 +0300

    Grouped the admin routes to make it easier to require authentication for admin routes.

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -6,74 +6,77 @@ const routes = [
     {
         path: '/',
         name: 'home',
-        meta: { public: true },
-        component: HomeView
+        component: HomeView,
+        meta: { public: true }
     },
-    // route level code-splitting
-    // this generates a separate chunk (About.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     {
         path: '/login',
         name: 'login',
         meta: { public: true },
-        component: () => import('../views/auth/LogIn.vue')
+        component: () => import('@/views/auth/LogIn.vue')
     },
     {
         path: '/signup',
         name: 'signup',
         meta: { public: true },
-        component: () => import('../views/auth/SignUp.vue')
+        component: () => import('@/views/auth/SignUp.vue')
     },
     {
         path: '/about',
         name: 'about',
         meta: { public: true },
-        component: () => import('../views/AboutView.vue')
+        component: () => import('@/views/AboutView.vue')
     },
     {
         path: '/shop',
         name: 'shop',
         meta: { public: true },
-        component: () => import('../views/ShopView.vue')
+        component: () => import('@/views/ShopView.vue')
     },
     {
         path: '/contact',
         name: 'contact',
         meta: { public: true },
-        component: () => import('../views/ContactView.vue')
+        component: () => import('@/views/ContactView.vue')
     },
     {
-        path: '/admin/dashboard',
-        name: 'admin_dashboard',
-        meta: {
-            requiresAuth: true
-        },
-        component: () => import('../views/admin/AdminDashboard.vue')
-    },
-    {
-        path: '/admin/categories',
-        name: 'admin_categories',
-        component: () => import('../views/admin/products/categories/CategoriesIndex.vue')
-    },
-    {
-        path: '/admin/categories/store',
-        name: 'new_category',
-        component: () => import('../views/admin/products/categories/AddCategory.vue')
-    },
-    {
-        path: '/admin/categories/:id/update',
-        name: 'update_category',
-        component: () => import('../views/admin/products/categories/UpdateCategory.vue'),
-        props: true
-    },
-    {
-        path: '/admin/products',
-        name: 'admin_products',
-        component: () => import('../views/admin/products/products/ProductsIndex.vue')
+        path: '/admin',
+        name: 'admin',
+        meta: { requiresAuth: true },
+        component: () => import('@/views/admin/AdminDashboard.vue'),
+        children: [
+            {
+                path: 'dashboard',
+                name: 'admin_dashboard',
+                component: () => import('@/views/admin/AdminDashboard.vue')
+            },
+            {
+                path: 'categories',
+                name: 'admin_categories',
+                component: () => import('@/views/admin/products/categories/CategoriesIndex.vue')
+            },
+            {
+                path: 'categories/store',
+                name: 'new_category',
+                component: () => import('@/views/admin/products/categories/AddCategory.vue')
+            },
+            {
+                path: 'categories/:id/update',
+                name: 'update_category',
+                props: true,
+                component: () => import('@/views/admin/products/categories/UpdateCategory.vue')
+            },
+            {
+                path: 'products',
+                name: 'admin_products',
+                component: () => import('@/views/admin/products/products/ProductsIndex.vue')
+            }
+        ]
     },
     {
         path: '/:pathMatch(.*)',
         name: '404',
+        meta: { public: true },
         component: () => import('@/views/404Page.vue')
     }
 ]
@@ -86,8 +89,6 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
     if (to.meta.requiresAuth && !store.state.token) {
         next({ name: 'login' })
-    } else if (to.meta.requiresGuest && store.state.user.token) {
-        next({ name: 'app.dashboard' })
     } else {
         next()
     }
